0e7d0af88635b77e499ce1b7e15d2ab55c2570d5
yeanjae0312 <yeanjae@naver.com>
2021-05-29 14:47:54 +0900

    add vue-cli start

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 위 내용과 같은 내용: 컴포넌트 명명법 종류 -->
+    <!-- <hello-world></hello-world>
+        <HelloWorld></HelloWorld>
+        <HelloWorld/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+    // 'hello-world': HelloWorld, // 위의 내용과 같은내용
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
